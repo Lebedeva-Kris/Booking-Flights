dd1e218234eb0d916213647e99bab103f75f39d8
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-26 22:33:31 +0300

    BookingRepository added

--- src/main/java/ru/netology/repository/BookingRepository.java
+++ src/main/java/ru/netology/repository/BookingRepository.java
@@ -1,4 +1,46 @@
 package ru.netology.repository;
 
+import ru.netology.domain.Booking;
+
 public class BookingRepository {
+    private Booking[] flights = new Booking[0];
+
+    public Booking[] findAll(){
+        return flights;
+    }
+
+    public void save(Booking flight) {
+        int length = flights.length + 1;
+        Booking[] tmp = new Booking[length];
+        System.arraycopy(flights, 0, tmp, 0, flights.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = flight;
+        flights = tmp;
+    }
+
+    public Booking findById(int id) {
+        for (Booking flight : flights) {
+            if (flight.getId() == id) {
+                return flight;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        int length = flights.length - 1;
+        Booking[] tmp = new Booking[length];
+        int index = 0;
+        for (Booking flight : flights) {
+            if (flight.getId() != id) {
+                tmp[index] = flight;
+                index++;
+            }
+        }
+        flights = tmp;
+    }
+
+    public void removeAll() {
+        flights = new Booking[0];
+    }
 }
